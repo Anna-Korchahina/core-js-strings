07807137d2bec858a352129134e36406f40ee1a4
Anna-Korchahina <korchagina.anna.v@gmail.com>
2023-11-18 22:38:26 +0200

    do task sumOfCodes

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -193,8 +193,15 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (typeof str !== 'string') {
+    return 0;
+  }
+  let res = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    res += str.charCodeAt(i);
+  }
+  return res;
 }
 
 /**
